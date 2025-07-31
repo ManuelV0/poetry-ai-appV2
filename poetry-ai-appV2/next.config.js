cdebb6a077288f6f2bffccc5d8c5243f7bc54422
ManuelV0 <manuel.voll69@icloud.com>
2025-07-31 15:26:43 +0200

    Update next.config.js

--- poetry-ai-appV2/next.config.js
+++ poetry-ai-appV2/next.config.js
@@ -1,16 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',
+  distDir: 'out',
+  images: {
+    unoptimized: true,
+    domains: ['lh3.googleusercontent.com'],
+  },
   experimental: {
     serverActions: true,
-    optimizePackageImports: ['@supabase/supabase-js']
-  },
-  images: {
-    domains: ['lh3.googleusercontent.com'],
-    unoptimized: true
+    optimizePackageImports: [
+      '@supabase/supabase-js',
+      '@radix-ui/react-dropdown-menu'
+    ],
+    serverComponentsExternalPackages: ['@supabase/supabase-js']
   },
   webpack: (config) => {
-    config.externals.push('@supabase/supabase-js');
+    config.externals.push({
+      '@supabase/supabase-js': 'supabase',
+    });
     return config;
   }
 }
