a6415543da7885871874622e835df031ce7155fd
ManuelV0 <manuel.voll69@icloud.com>
2025-07-31 16:27:52 +0200

    Update next.config.js

--- poetry-ai-appV2/next.config.js
+++ poetry-ai-appV2/next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  distDir: 'out',
+  distDir: process.env.NETLIFY ? '.next' : 'out', // Differenzia locale e Netlify
+  trailingSlash: true,
   images: {
     unoptimized: true
   }
 }
-
 module.exports = nextConfig
