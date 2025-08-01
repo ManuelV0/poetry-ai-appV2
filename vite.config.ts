1a71a10c52eac2c8ba1974f5c5f33d5c7dea1e82
ManuelV0 <manuel.voll69@icloud.com>
2025-08-01 16:40:45 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
+  resolve: {
+    alias: {
+      '/src': path.resolve(__dirname, 'src'), // Optional: solo se vuoi usare /src nei tuoi import
+    },
   },
-  server: {
-    port: 3000,
-  },
-});
+})
